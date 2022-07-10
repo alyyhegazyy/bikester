a13545c623b5f973fcf8c32cf96cb0c8f33baed8
miladatef <meladattef2017@gmail.com>
2022-07-10 23:01:49 +0200

    add geocoding api key

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -1,3 +1,2 @@
-String apiKey = 'YOUR_ANDROID/iOS_APK_API_KEY';
-String geocodingApi = 'YOUR_GEOCODING_API_KEY';
+String geocodingApi = 'AIzaSyDGEEKJYKi_WIegFKiRLUcfr064ctP61qI';
 //(This geocoding api also works for places api and directions api if you have restricted the api key with these 3 services)
